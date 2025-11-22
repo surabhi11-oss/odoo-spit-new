1e537c28730a5752cbfcaa5a517d111a2bc06bbc
Pratik Devang <devangpratik8@gmail.com>
2025-11-22 09:23:43 +0530

    Initial Commit

---
+++ src/main/java/com/stockmaster/stockmaster_backend/StockmasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockmaster.stockmaster_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmasterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockmaster/stockmaster_backend/StockmasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmaster.stockmaster_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
